5835188f284e2ba040915c639201ee6fdfdc99f2
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2022-01-04 23:40:43 -0300

    dependencies and basic chakra UI structure

---
+++ styles/Theme/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const themeApp = extendTheme({
+  colors: {
+    primary: '#5D001E',
+    secondary: '#E3E2DF',
+    tertiary: '#E3AFBC',
+    text: '#9A1750',
+    fourth: '#EE407C',
+  },
+  components: {},
+});
